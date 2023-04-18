fcaf98aa57c176131460f0f5600cd67c45f2a7fc
Rustam <tabarzin@gmail.com>
2023-04-18 21:31:29 +0300

    feat: add callback modal and call a specialist modal

--- src/js/main.js
+++ src/js/main.js
@@ -12,8 +12,29 @@ span.onclick = function () {
   measurerModal.style.display = "none";
 };
 
+/* “Заказать обратный звонок” и “Спросите у нашего специалиста” */
+
+const callOrAskModal = document.getElementsByClassName("popup")[0];
+const callOrAskBtn1 = document.getElementsByClassName("phone_link")[0];
+const callOrAskBtn2 = document.getElementsByClassName("phone_link")[1];
+const closeCallOrAsk = document.getElementById("closeCallOrAsk");
+
+callOrAskBtn1.onclick = function () {
+  callOrAskModal.style.display = "block";
+};
+
+callOrAskBtn2.onclick = function () {
+  callOrAskModal.style.display = "block";
+};
+
+closeCallOrAsk.onclick = function () {
+  callOrAskModal.style.display = "none";
+};
+
 window.onclick = function (event) {
   if (event.target == measurerModal) {
     measurerModal.style.display = "none";
+  } else if (event.target == callOrAskModal) {
+    callOrAskModal.style.display = "none";
   }
 };
