7766ce2d69e521b03884f1885c70e6d2b3c14470
Rustam <tabarzin@gmail.com>
2023-06-02 21:33:40 +0300

    refactor: move all variables to the top

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,8 +1,32 @@
 /* Вызов замерщика */
-
 const measurerModal = document.querySelector(".popup_engineer");
 const measurerBtn = document.getElementById("callMeasurer");
 const span = document.getElementById("popup_close");
+
+/* “Заказать обратный звонок” и “Спросите у нашего специалиста” */
+
+const callModal = document.querySelector(".popup");
+const callModalBtns = document.querySelectorAll(".phone_link");
+const closeCallModal = document.getElementById("closeCallOrAsk");
+
+/* Калькулятор */
+
+const calcBtns = document.querySelectorAll(".glazing_price_btn");
+const calcModal = document.querySelector(".popup_calc");
+const closeCalcModals = document.querySelectorAll(".popup_calc_close");
+const iconsContainer = document.querySelector(".balcon_icons");
+const bigImgs = document.querySelectorAll(".big_img img");
+const button = document.querySelector(".popup_calc_button");
+const modalToClose = document.querySelector(".popup_calc");
+const calcProfile = document.querySelector(".popup_calc_profile");
+const calcProfileClose = document.querySelector(".popup_calc_profile_close");
+const checkboxes = document.querySelectorAll('input[name="checkbox-test"]');
+const secondButton = document.querySelector(".popup_calc_profile_button");
+const secondModalToClose = document.querySelector(".popup_calc_profile");
+const calcEnd = document.querySelector(".popup_calc_end ");
+const calcEndClose = document.querySelector(".popup_calc_end_close");
+
+/* Вызов замерщика */
 
 measurerBtn.addEventListener("click", () => {
   measurerModal.style.display = "block";
@@ -13,10 +37,6 @@ span.addEventListener("click", () => {
 });
 
 /* “Заказать обратный звонок” и “Спросите у нашего специалиста” */
-
-const callModal = document.querySelector(".popup");
-const callModalBtns = document.querySelectorAll(".phone_link");
-const closeCallModal = document.getElementById("closeCallOrAsk");
 
 callModalBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -29,10 +49,6 @@ closeCallModal.addEventListener("click", () => {
 });
 
 /* Калькулятор */
-
-const calcBtns = document.querySelectorAll(".glazing_price_btn");
-const calcModal = document.querySelector(".popup_calc");
-const closeCalcModals = document.querySelectorAll(".popup_calc_close");
 
 calcBtns.forEach((button) => {
   button.addEventListener("click", () => {
@@ -48,9 +64,6 @@ closeCalcModals.forEach((close) => {
 
 /* Калькулятор. Выбор иконок в модальном окне */
 
-const iconsContainer = document.querySelector(".balcon_icons");
-const bigImgs = document.querySelectorAll(".big_img img");
-
 iconsContainer.addEventListener("click", (event) => {
   bigImgs.forEach((img) => {
     img.style.display = "none";
@@ -63,10 +76,6 @@ iconsContainer.addEventListener("click", (event) => {
 
 /* Калькулятор. При нажатии на кнопку "далее" */
 
-const button = document.querySelector(".popup_calc_button");
-const modalToClose = document.querySelector(".popup_calc");
-const calcProfile = document.querySelector(".popup_calc_profile");
-
 button.addEventListener("click", () => {
   modalToClose.style.display = "none";
 
@@ -74,14 +83,12 @@ button.addEventListener("click", () => {
   calcProfile.style.display = "block";
 });
 
-const calcProfileClose = document.querySelector(".popup_calc_profile_close");
 calcProfileClose.addEventListener("click", () => {
   calcProfile.style.display = "none";
 });
 
 /* Калькулятор. Запрет за выбор двух чекбоксов */
 
-const checkboxes = document.querySelectorAll('input[name="checkbox-test"]');
 for (let i = 0; i < checkboxes.length; i++) {
   checkboxes[i].addEventListener("change", () => {
     if (checkboxes[i].checked) {
@@ -96,10 +103,6 @@ for (let i = 0; i < checkboxes.length; i++) {
 
 /* Калькулятор. При нажатии на вторую кнопку "далее" */
 
-const secondButton = document.querySelector(".popup_calc_profile_button");
-const secondModalToClose = document.querySelector(".popup_calc_profile");
-const calcEnd = document.querySelector(".popup_calc_end ");
-
 secondButton.addEventListener("click", () => {
   // Get the current modal window element
   secondModalToClose.style.display = "none";
@@ -108,7 +111,6 @@ secondButton.addEventListener("click", () => {
   calcEnd.style.display = "block";
 });
 
-const calcEndClose = document.querySelector(".popup_calc_end_close");
 calcEndClose.addEventListener("click", () => {
   calcEnd.style.display = "none";
 });
